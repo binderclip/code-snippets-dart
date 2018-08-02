d414bc671d1fdd024beb14be3729395573419a8b
clip <binderclip2014@gmail.com>
2018-08-02 22:45:11 +0800

    objects

---
+++ built_in_types/objects.dart
@@ -0,0 +1,25 @@
+printType(a) {
+  print(a.runtimeType);
+}
+
+testPrintType() {
+  print('=== testPrintType ===');
+  var a = 1;
+  var b = 1.1;
+  var c = 'c';
+  var d = [1];
+  var e = new Map();
+  var f = '\u{1f44f}';
+  var g = new Runes('\u{1f44f}');
+  printType(a);
+  printType(b);
+  printType(c);
+  printType(d);
+  printType(e);
+  printType(f);
+  printType(g);
+}
+
+main() {
+  testPrintType();
+}
