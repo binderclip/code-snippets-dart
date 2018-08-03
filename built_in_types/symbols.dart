32e04f5661aa668c4b7e9bb7e67ede766bfafa3a
clip <binderclip2014@gmail.com>
2018-08-03 09:04:30 +0800

    symbols

---
+++ built_in_types/symbols.dart
@@ -0,0 +1,8 @@
+main() {
+  print('pass symbols for now');
+}
+
+// https://www.dartlang.org/guides/language/language-tour#symbols
+// https://api.dartlang.org/dev/dart-core/Symbol-class.html
+// https://www.tutorialspoint.com/dart_programming/dart_programming_symbol.htm
+// https://api.dartlang.org/stable/1.24.3/dart-mirrors/MirrorsUsed/symbols.html
