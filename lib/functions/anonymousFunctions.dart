88ef57d8347c92ce1704eaa4a86f09e34cb332b0
clip <binderclip2014@gmail.com>
2018-08-04 20:00:20 +0800

    anonymous functions

---
+++ lib/functions/anonymousFunctions.dart
@@ -0,0 +1,7 @@
+main() {
+  var list = ['apples', 'bananas', 'oranges'];
+  list.forEach((item) {
+    print('${list.indexOf(item)}: $item');
+  });
+  list.forEach((item) => print('${list.indexOf(item)}: $item'));
+}
