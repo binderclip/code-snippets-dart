c5568b9dfda649375f768048f8ac49f073a571e2
clip <binderclip2014@gmail.com>
2018-08-05 19:31:13 +0800

    return values

---
+++ lib/functions/returnValues.dart
@@ -0,0 +1,20 @@
+foo() {}
+
+void bar() {}
+
+baz() {
+  return;
+}
+
+int qux() {
+  return 1;
+}
+
+main() {
+  print(foo());
+  print(bar());
+  print(baz());
+  print(qux());
+}
+
+// https://www.dartlang.org/guides/language/language-tour#return-values
