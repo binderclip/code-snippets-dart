e86127f07d945ed5d415fda881bcc604e676919c
zhoubokun <zhoubokun@bytedance.com>
2019-08-10 15:32:43 +0800

    list
    Change-Id: I57fa2ecc026e047c9a67582411d10b5a04646417

--- demo/lang/built_in_types/lists.dart
+++ demo/lang/built_in_types/lists.dart
@@ -27,14 +27,29 @@ listIndexDemo() {
 }
 
 initList() {
+  printFuncName();
+  print('== null list ==');
   List<int> l;
   print(l);
   // l.add(1); // NoSuchMethodError: The method 'add' was called on null.
+  print('== fixed length list ==');
   l = new List(5);
   print(l);
   // l.add(1); // Unsupported operation: Cannot add to a fixed-length list
   l[0] = 1;
   print(l);
+  print('== variable length list ==');
+  l = new List();
+  print(l);
+  l.add(2);
+  print(l);
+  print('== variable with init length list ==');
+  l = new List()..length = 2;
+  print(l); // [null, null]
+  l.add(3);
+  l.add(4);
+  l.add(5);
+  print(l); // [null, null, 3, 4, 5]
 }
 
 main() {
@@ -46,3 +61,5 @@ main() {
 // https://www.dartlang.org/guides/language/language-tour#lists
 // https://www.dartlang.org/guides/language/language-tour#generics
 // https://www.dartlang.org/guides/libraries/library-tour#collections
+// Dart入门—集合类型 - 咖啡花园 - CSDN博客
+// https://blog.csdn.net/hekaiyou/article/details/51374093
