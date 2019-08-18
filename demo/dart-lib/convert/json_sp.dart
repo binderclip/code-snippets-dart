77bb1d1cd9ffbf5a2e2c31c415899cfdbb0f8edb
zhoubokun <zhoubokun@bytedance.com>
2019-08-19 00:38:36 +0800

    json
    Change-Id: Ie5c65a157a9e5d102d1ba9c9a1563c2e23d63f71

---
+++ demo/dart-lib/convert/json_sp.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+import '../../lang/classes/getting_an_object_s_type.dart';
+import '../../lib/stack_trace/stack_trace.dart';
+
+final userJs = """{
+  "name": "John Smith",
+  "email": "john@example.com"
+}""";
+
+jsonDecodeDemo() {
+  printFuncName();
+  var user = jsonDecode(userJs);
+  print(user);
+  printType(user); // _InternalLinkedHashMap<String, dynamic>
+}
+
+jsonEncodeDemo() {
+  printFuncName();
+  var gifts = {
+    // Key:    Value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 'golden rings'
+  };
+  print(gifts);
+  print(jsonEncode(gifts));
+}
+
+class User {
+  final String name;
+  final String email;
+
+  User(this.name, this.email);
+
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        email = json['email'];
+
+  Map<String, dynamic> toJson() =>
+    {
+      'name': name,
+      'email': email,
+    };
+}
+
+jsonDecodeModel() {
+  printFuncName();
+  var user = User.fromJson(jsonDecode(userJs));
+  print('user: ${user}, .name: ${user.name}, .email: ${user.email}');
+}
+
+jsonEncodeModel() {
+  printFuncName();
+  var user = User('Foo', 'bar@baz.com');
+  var userJs = jsonEncode(user);
+  print(userJs);
+}
+
+main() {
+  jsonDecodeDemo();
+  jsonEncodeDemo();
+  jsonDecodeModel();
+  jsonEncodeModel();
+}
---
+++ demo/dart-lib/convert/readme.md
@@ -0,0 +1,5 @@
+# readme
+
+## json
+
+- [JSON and serialization - Flutter](https://flutter.dev/docs/development/data-and-backend/json)
