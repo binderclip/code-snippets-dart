b1113420206358efbd24f0f416febce82a71ad24
zhoubokun <zhoubokun@bytedance.com>
2019-08-24 15:12:03 +0800

    time
    Change-Id: I0e3d3e1133b8259e847022ccb9dd4fb204c05aad

--- lib/time/time.dart
+++ lib/time/time.dart
@@ -1,8 +1,28 @@
+import 'package:dart_code_snippets/stack_trace/stack_trace.dart';
+
 int timestamp() {
   return DateTime.now().millisecondsSinceEpoch ~/ 1000;
 }
 
-main() {
+currentTimeDemo() {
+  printFuncName();
+  print(DateTime.now());
+}
+
+readTimestampDemo() {
+  printFuncName();
   print(new DateTime.now().millisecondsSinceEpoch);
   print(timestamp());
+}
+
+timestampToTimeDemo() {
+  printFuncName();
+  var dt = DateTime.fromMillisecondsSinceEpoch(1566630660599);
+  print(dt);
+}
+
+main() {
+  currentTimeDemo();
+  readTimestampDemo();
+  timestampToTimeDemo();
 }
