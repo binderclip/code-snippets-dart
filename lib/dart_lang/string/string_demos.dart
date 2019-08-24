0fa6b6ea5b003fe2907afc4396e185e78845269d
zhoubokun <zhoubokun@bytedance.com>
2019-08-24 15:39:28 +0800

    string
    Change-Id: I0cadcbf9cbc1f17b64268a1898fea81b7e9f222e

---
+++ lib/dart_lang/string/string_demos.dart
@@ -0,0 +1,13 @@
+import 'package:dart_code_snippets/stack_trace/stack_trace.dart';
+
+trimDemo() {
+  printFuncName();
+  var s = "   \ta b\t\n   \n";
+  var ts = s.trim();
+  print('${s.length}, "$s"');
+  print('${ts.length}, "$ts"');
+}
+
+main() {
+  trimDemo();
+}
